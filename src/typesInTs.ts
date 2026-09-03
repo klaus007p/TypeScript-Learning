df9f029f385fd01df308c8aca3e2930414815938
Prabhat Pathak <klaus007p@gmail.com>
2026-09-03 21:18:03 +0530

    Learned about the basic types in typescript

--- src/typesInTs.ts
+++ src/typesInTs.ts
@@ -1,4 +1,35 @@
+// Types In TS ...
+
 let drink = "chai";
 
 let chaiFlavour: string = "Masala Chai"
 chaiFlavour = "GingerTea";
+let nothing: null = null;
+
+
+
+let username: string = "Alex";  // All are type specific we have to define the types of variables 
+let age: number = 21;
+let isDeveloper = true;
+
+let skills: string[] = ["React", "TypeScript"];
+
+let user: {
+    name: string;
+    age: number;
+} = {
+    name: "Alex",
+    age: 31
+}
+
+
+// Array Types in ts
+
+let numbers: number[] = [1, 2, 3, 4];
+
+let names: string[] = ["John", "Alex", "Don"];
+
+let num: Array<number> = [1, 2, 3] // Another way to declare arrays
+
+
+
